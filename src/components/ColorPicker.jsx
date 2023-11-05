4cde2f0bc9aaaed22f84746612482ea6fdac6d92
Colton Patch <coltondp@gmail.com>
2023-11-04 23:32:34 -0700

    Color picker works

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 import { SketchPicker } from 'react-color';
 
 class ColorPicker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     state = {
-        background: '#fff',
+        background: 'rgb',
     };
 
     handleChangeComplete = (color) => {
-        this.setState({ background: color.hex });
+        this.setState({ background: color.rgb });
+    };
+
+    parseRgb = (rgb) => {
+        let rgbArr = [rgb.r, rgb.g, rgb.b];
+        return rgbArr;
     };
 
     render() {
         return (
+            <>
             <SketchPicker
                 color={this.state.background}
                 onChangeComplete={this.handleChangeComplete}
             />
+
+            <button onClick = {() => this.props.request(this.parseRgb(this.state.background))}> Worjkdshh</button>
+            </>
         );
     }
+
+    
 }
 
 export default ColorPicker;
