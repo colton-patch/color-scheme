afbb10c61fd751f42f9b06a6c755eb55ef749249
Zeb <moffat.zeb@gmail.com>
2023-11-05 03:53:22 -0700

    canvas changes

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,43 +1,52 @@
-import React, { useState } from 'react';
-import { ReactSketchCanvas } from 'react-sketch-canvas';
+import React, { useState } from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
 const Canvas = ({ fiveColors }) => {
-    const colorArray = Object.values(fiveColors);
-    const [currentColor, setCurrentColor] = useState(colorArray[0]);
+  const colorArray = Object.values(fiveColors);
+  const [currentColor, setCurrentColor] = useState(colorArray[0]);
 
-    const handleColorChange = (color) => {
-        setCurrentColor(color);
-    };
+  const handleColorChange = (color) => {
+    setCurrentColor(color);
+  };
 
-    return (
-        <div className='centerParent'>
-            <div>
-                {colorArray.map((color, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
-                            width: '30px',
-                            height: '30px',
-                            border: '1px solid #ccc',
-                            display: 'inline-block',
-                            cursor: 'pointer',
-                            marginRight: '5px',
-                        }}
-                        onClick={() => handleColorChange(color)}
-                    ></div>
-                ))}
-            </div >
-            <div width='3rem' height='50rem'>
-                <ReactSketchCanvas
-                    strokeColor={`rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`}
-                    strokeWidth={20}
-                    width="30%"
-                    display='flex'
-                />
-            </div>
+  const styles = {
+    border: "1rem solid #9c9c9c",
+    borderRadius: "2",
+    height: '6rem',
+  };
+
+  return (
+    <div className="centerParent">
+      <div>
+        {colorArray.map((color, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+              width: "2rem",
+              height: "2rem",
+              border: "1px solid #ccc",
+              display: "inline-block",
+              cursor: "pointer",
+              marginRight: "5px",
+            }}
+            onClick={() => handleColorChange(color)}
+          ></div>
+        ))}
+      </div>
+      <div className="canvasArea">
+        <div width="3rem" height="50rem">
+          <ReactSketchCanvas
+            strokeColor={`rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`}
+            strokeWidth={20}
+            width="85%"
+            display="flex"
+            style={styles}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Canvas;
